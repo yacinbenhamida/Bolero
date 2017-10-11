361dd9e0de5bb0ad723d609e7ef68220fd427862
yacinbenhamida <yacinbenhamida@hotmail.fr>
2017-10-11 20:39:31 +0100

    Creation de la classe User

---
+++ Bolero/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolero
+{
+    class User
+    {
+        private int id;
+        private string name;
+        private string password;
+        private string secretQuestion;
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Password { get; set; }
+        public String SecretQuestion { get; set; }
+        public string toString() { return "Id : " + Id + " Nom : " + Name + " PW : " + Password + " SQUESTION" +SecretQuestion; }
+    }
+}
