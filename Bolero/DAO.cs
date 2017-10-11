d103837d0ff74fd0ddb2d6b2a477e3d74f95d463
yacinbenhamida <yacinbenhamida@hotmail.fr>
2017-10-11 20:32:43 +0100

    Adding DAO

---
+++ Bolero/DAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolero
+{
+    interface DAO<T>
+    {
+        public bool add(T element);
+        public bool delete(T element);
+        public List<T> getAll();
+        public T findById(int id);
+        public T findByName(string name);
+        public bool update(T element);
+    }
+}
