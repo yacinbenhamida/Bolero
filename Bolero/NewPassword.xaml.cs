8fa2fefec892a2e9638ea3466974ccf776229c24
zakariabh <zikoubenhmida@gmail.com>
2017-10-11 23:22:27 +0200

    Interface Authentification

---
+++ Bolero/NewPassword.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bolero
+{
+    /// <summary>
+    /// Logique d'interaction pour Window3.xaml
+    /// </summary>
+    public partial class NewPassword : Window
+    {
+        public NewPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblDate.Content = DateTime.Now.ToShortDateString();
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            lblHeure.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
