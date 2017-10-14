8e81cd387cdcba9afa551a6c62220636b65f7ceb
yacinbenhamida <yacinbenhamida@hotmail.fr>
2017-10-14 11:10:36 +0100

    fixing DB problem

---
+++ Bolero/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nom] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [questionsecrete] VARCHAR(50) NOT NULL
+)
--- Bolero/obj/Release/Bolero_Content.g.i.cs
+++ Bolero/obj/Release/Bolero_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/user.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bolerodb.mdf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bolerodb_log.ldf")]
 
 
