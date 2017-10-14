1d2de1c3abed46e44197307d5d4a39e6c710be87
zakariabh <zikoubenhmida@gmail.com>
2017-10-14 11:35:01 +0200

    Revert "Merge branch 'Authentification' of https://github.com/yacinbenhamida/Bolero into Authentification"
    This reverts commit 61c74e1b6cc226dc43fa396e1fd54a17ee5ca466, reversing
    changes made to df00449faab6452e707fd48f5445d10b2daa2142.

--- Bolero/dbo.staff.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[staff] (
-    [id]              INT          NOT NULL,
-    [username]        VARCHAR (20) NOT NULL,
-    [password]        VARCHAR (20) NOT NULL,
-    [questionsecrete] VARCHAR (20) NOT NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
--- SQLDBBolero.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE staff (
-id int primary key,
-username VARCHAR(20),
-password VARCHAR(20),
-questionsecrete VARCHAR(20)
-);
