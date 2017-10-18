5d87c3c253d8b88004095d61ab7f528cf85ce157
zakariabh <zikoubenhmida@gmail.com>
2017-10-18 19:44:27 +0200

    Interface Gestion-Article

---
+++ Bolero/ModifierPlat.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bolero
+{
+    /// <summary>
+    /// Logique d'interaction pour ModifierPlat.xaml
+    /// </summary>
+    public partial class ModifierPlat : Window
+    {
+        public ModifierPlat()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
