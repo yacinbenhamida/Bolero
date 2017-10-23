69dea7988946fd32769a54a66f3c1c93351be203
maryemlouhichi <32373197+maryemlouhichi@users.noreply.github.com>
2017-10-23 12:13:32 +0200

    classe table et classe commande done

--- Bolero/BL/Commande.cs
+++ Bolero/BL/Commande.cs
@@ -8,5 +8,21 @@ namespace Bolero.BL
 {
     class Commande
     {
+        private int IdCommande { get; set; }
+        private int NumTable { get; set; }
+        private DateTime DateCommande { get; set; }
+        private int IdArticle { get; set; }
+        private string NomServeur { get; set; }
+        private int Id { get; set; }
+
+        public Commande(int idcommande, int numtable, DateTime datecommande, int idarticle, string nomserveur, int id)
+        {
+            this.IdCommande = idcommande;
+            this.NumTable = numtable;
+            this.DateCommande = datecommande;
+            this.IdArticle = idarticle;
+            this.NomServeur = nomserveur;
+            this.Id = id;  
+        }
     }
 }
---
+++ Bolero/BL/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolero.BL
+{
+    class Table
+    {
+        private int NumTable { get; set; }
+        private Boolean Etat { get; set; }
+
+        public Table(int num, Boolean etar)
+        {
+            this.NumTable = num;
+            this.Etat = Etat;
+        }
+    }
+}
