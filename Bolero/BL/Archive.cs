2d6d2d04ba256fc3b11338b0d00d1d5e14453651
zizo1234 <32396859+zizo1234@users.noreply.github.com>
2017-11-19 17:56:28 +0100

    creation class Archive,ArchiveDAO

---
+++ Bolero/BL/Archive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolero.BL
+{
+    class Archive
+    {
+           public Archive() { }
+        public int idCommande { get; set; }
+        public int numTable { get; set; }
+        public DateTime Datecommande { get; set; }
+        public string nomServeur { get; set; }
+        public string caissier { get; set; }
+        public float sumprix { get; set; }
+
+
+        public Archive() { }
+        public Archive(int idCommande, int numTable, DateTime Datecommande, string nomServeur,string caissier, float sumprix) {
+
+            this.idCommande = idCommande;
+            this.numTable = numTable;
+            this.Datecommande = Datecommande;
+            this.nomServeur = nomServeur;
+            this.caissier = caissier;
+            this.sumprix = sumprix;
+        
+        
+        }
+
+    }
+
+    
+}
